af0b24ba1be01fff816a5bbf82230fbb69dd191d
youssefea <youcefea99@gmail.com>
2025-04-29 00:00:55 +0200

    add sub account compatibility

--- smart-wallet/coin-your-joke/src/wagmi.ts
+++ smart-wallet/coin-your-joke/src/wagmi.ts
@@ -1,15 +1,32 @@
+import { parseEther } from "viem";
+import { toHex } from "viem";
 import { http, cookieStorage, createConfig, createStorage } from "wagmi";
 import { base, baseSepolia } from "wagmi/chains";
 import { coinbaseWallet } from "wagmi/connectors";
 
 export const cbWalletConnector = coinbaseWallet({
-  appName: "Smart Wallet Zora Coiner",
-  preference: "smartWalletOnly",
+  appName: "Coin Your Bangers - Sub Account Demo",
+  preference: {
+    keysUrl: "https://keys-dev.coinbase.com/connect",
+    options: "smartWalletOnly",
+  },
+  subAccounts: {
+    enableAutoSubAccounts: true,
+    defaultSpendLimits: {
+      84532: [
+        {
+          token: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+          allowance: toHex(parseEther('0.01')),
+          period: 86400,
+        },
+      ],
+    },
+  },
 });
 
 export function getConfig() {
   return createConfig({
-    chains: [base, baseSepolia],
+    chains: [ baseSepolia],
     connectors: [cbWalletConnector],
     storage: createStorage({
       storage: cookieStorage,
@@ -17,7 +34,6 @@ export function getConfig() {
     ssr: true,
     transports: {
       [baseSepolia.id]: http(),
-      [base.id]: http(),
     },
   });
 }
